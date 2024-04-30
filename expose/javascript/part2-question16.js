7c2d9cb6638b574a1eeac9cb5a7b013ea668c9fa
Amdadul Haque <122418243+Amdau1@users.noreply.github.com>
2024-04-29 23:08:24 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,5 +1,5 @@
-for (let key in stat) {
-    if (key[0] === 'r' || stat[key] % 2 !== 0) {
+for (let key in statistics) {
+    if (key[0] === 'r' || statistics[key] % 2 !== 0) {
         console.log(stat[key]);
     }
 }
